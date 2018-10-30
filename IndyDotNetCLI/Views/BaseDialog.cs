193f9f1d9e3818766b6df5926556830da9d20c9f
matt raffel <matt.raffel@evernym.com>
2018-10-30 10:49:18 -0600

    work on new base dialog
    Signed-off-by: matt raffel <matt.raffel@evernym.com>

---
+++ IndyDotNetCLI/Views/BaseDialog.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Terminal.Gui;
+
+namespace IndyDotNetCLI.Views
+{
+    public abstract class BaseDialog : Window
+    {
+        protected static int PADDING = 1;
+        protected int _selectedButton = 0;
+        protected List<Button> _buttons = new List<Button>();
+
+        public BaseDialog(string title, int width = 60, int height = 40) : base(title, PADDING)
+        {
+            X = Pos.Center();
+            Y = Pos.Center();
+            Width = width;
+            Height = height;
+            ColorScheme = Colors.Dialog;
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            const int borderWidth = 2;
+            int buttonSpace = 0;
+            int maxHeight = 0;
+
+            foreach (var b in _buttons)
+            {
+                buttonSpace += b.Frame.Width + 1;
+                maxHeight = Math.Max(maxHeight, b.Frame.Height);
+            }
+
+            var start = (Frame.Width - borderWidth - buttonSpace) / 2;
+
+            var y = Frame.Height - borderWidth - maxHeight - 1 - PADDING;
+            foreach (var b in _buttons)
+            {
+                var bf = b.Frame;
+                b.Frame = new Rect(start, y, bf.Width, bf.Height);
+                start += bf.Width + 1;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buttonId"></param>
+        /// <param name="buttonText"></param>
+        /// <param name="clickEvent">delegate for the click event, if null, default is used which will close the dialog</param>
+        protected void CreateButton(int buttonId, string buttonText, Action clickEvent = null)
+        {
+            Action clickAction = clickEvent;
+            if (null == clickAction)
+            {
+                clickAction = delegate
+                {
+                    _selectedButton = buttonId;
+                    this.Running = false;
+                };
+            }
+
+            var btn = new Button(buttonText);
+            btn.Clicked += clickAction;
+            AddButton(btn);
+        }
+
+        protected void AddButton(Button button)
+        {
+            _buttons.Add(button);
+            Add(button);
+        }
+
+        protected int ComputeLeftOfCenter(int widthOfWindow, int displayWidth)
+        {
+            int center = (widthOfWindow / 2);
+            int halfOfDisplayWidth = (displayWidth / 2);
+            var leftOfCenter = center - halfOfDisplayWidth;
+            if (0 >= leftOfCenter) leftOfCenter = 5;
+
+            // widthOfWindow: 60 - halfOfDisplayWidth:6 - leftOfCenter:(
+            System.Diagnostics.Debug.WriteLine($"widthOfWindow: {widthOfWindow} - halfOfDisplayWidth:{halfOfDisplayWidth} - leftOfCenter:{leftOfCenter}");
+
+            return leftOfCenter;
+        }
+    }
+}
--- IndyDotNetCLI/Views/ListDialog.cs
+++ IndyDotNetCLI/Views/ListDialog.cs
@@ -4,13 +4,9 @@ using Terminal.Gui;
 
 namespace IndyDotNetCLI.Views
 {
-    public class ListDialog : Window
+    public class ListDialog : BaseDialog
     {
-        private List<Button> _buttons = new List<Button>();
-        private static int PADDING = 1;
-        private int _selectedButton = 0;
-
-        public ListDialog(string title, string message, System.Collections.IList source, int width = 60, int height = 40) : base(title, PADDING)
+        public ListDialog(string title, string message, System.Collections.IList source, int width = 60, int height = 40) : base(title)
         {
             X = Pos.Center();
             Y = Pos.Center();
@@ -18,16 +14,8 @@ namespace IndyDotNetCLI.Views
             Height = height;
             ColorScheme = Colors.Dialog;
 
-            var ok = new Button(DialogConstants.ButtonTexts[DialogConstants.OK]);
-            ok.Clicked += delegate
-            {
-                _selectedButton = DialogConstants.OK;
-                this.Running = false;
-            };
-
-            _buttons.Add(ok);
-            Add(ok);
-
+            CreateButton(DialogConstants.OK, DialogConstants.ButtonTexts[DialogConstants.OK]);
+            
             var textLabel = new Label(ComputeLeftOfCenter(width, message.Length), 1, $"{message}");
             Add(textLabel);
 
@@ -51,49 +39,11 @@ namespace IndyDotNetCLI.Views
         /// <summary>
         /// Show the edit field dialog
         /// </summary>
-        /// <returns>int, the button chosen.  1 for OK, 2 for cancel.  string, user inputted data.</returns>
-        public (int, string) Show()
+        /// <returns>int, the button chosen.</returns>
+        public int Show()
         {
             Application.Run(this);
-            return (_selectedButton, string.Empty);
-        }
-
-        public override void LayoutSubviews()
-        {
-            base.LayoutSubviews();
-
-            const int borderWidth = 2;
-            int buttonSpace = 0;
-            int maxHeight = 0;
-
-            foreach (var b in _buttons)
-            {
-                buttonSpace += b.Frame.Width + 1;
-                maxHeight = Math.Max(maxHeight, b.Frame.Height);
-            }
-
-            var start = (Frame.Width - borderWidth - buttonSpace) / 2;
-
-            var y = Frame.Height - borderWidth - maxHeight - 1 - PADDING;
-            foreach (var b in _buttons)
-            {
-                var bf = b.Frame;
-                b.Frame = new Rect(start, y, bf.Width, bf.Height);
-                start += bf.Width + 1;
-            }
-        }
-
-        private int ComputeLeftOfCenter(int widthOfWindow, int displayWidth)
-        {
-            int center = (widthOfWindow / 2);
-            int halfOfDisplayWidth = (displayWidth / 2);
-            var leftOfCenter = center - halfOfDisplayWidth;
-            if (0 >= leftOfCenter) leftOfCenter = 5;
-
-            // widthOfWindow: 60 - halfOfDisplayWidth:6 - leftOfCenter:(
-            System.Diagnostics.Debug.WriteLine($"widthOfWindow: {widthOfWindow} - halfOfDisplayWidth:{halfOfDisplayWidth} - leftOfCenter:{leftOfCenter}");
-
-            return leftOfCenter;
-        }
+            return _selectedButton;
+        }       
     }
 }
